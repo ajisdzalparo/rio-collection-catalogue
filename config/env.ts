452e4869e32925398679c9414ab4cb18d8abafcb
Ajis Dzalparo <ajisdzalparo22@gmail.com>
2026-09-11 18:03:09 +0700

    fix(recaptcha): remove hardcoded test key fallback

--- config/env.ts
+++ config/env.ts
@@ -2,8 +2,7 @@ const apiUrl = process.env.NEXT_PUBLIC_API_URL || '/api';
 const backendUrl = process.env.BACKEND_API_URL || '/api';
 const minioEndpoint = process.env.MINIO_ENDPOINT || 'http://localhost:9000';
 const minioBucketName = process.env.MINIO_BUCKET_NAME || 'rio-collection-bucket';
-const recaptchaSiteKey =
-  process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY || '6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI';
+const recaptchaSiteKey = process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY || '';
 
 export const env = {
   apiUrl,
